a2af91ad8285676e5c5a9ea4299a1e5c893763f0
hmgene <human.gim@gmail.com>
2025-03-31 11:15:42 -0400

    anno

--- 03-sum-methylkit.sh
+++ 03-sum-methylkit.sh
@@ -6,6 +6,21 @@ results/E18pt5_vs_Week4_diff_25p_05q_hypo.tsv
 results/Week4_vs_2-year_diff_25p_05q_hyper.tsv
 results/Week4_vs_2-year_diff_25p_05q_hypo.tsv
 )
+fn(){
+    #annotatePeaks.pl $1 mm10 > $1.tmp
+    if [ -f $1.tmp ];then return;fi
+    mv $2 $1.tmp
+    R --no-save -e 'tt=read.table("'$1'",header=T)
+    colnames(tt)=c("Chr","Start","End", colnames(tt)[4:ncol(tt)])
+    tt1=read.csv("'$1.tmp'",header=T,sep="\\t");
+    tt1$Start = tt1$Start -1;
+    tt=merge(tt,tt1)
+    write.table(file="'$2'",tt,col.names=T,row.name=F,quote=F,sep="\\t")
+    '
+    rm $1.tmp
+};export -f fn;
+#parallel fn {} {.}_anno.tsv ::: ${input[@]};
+
 
 for f in ${input[@]};do
     s=${f#*/};s=${s%_diff*}
