f6abebb95dcc11ff04b70e5572de45cd35145f6d
hmgene <human.gim@gmail.com>
2025-04-22 14:11:37 -0400

    1

--- 2025-04-11.sh
+++ 2025-04-11.sh
@@ -9,22 +9,22 @@ echo '
 
 Filtered out E1, E2, W1, Y1, Y2
 
-![sample_correlation](results/2025-04-11/merged_anova_pval005_cor.png )
+![sample_correlation](merged_anova_pval005_cor.png )
 
 ## ANOVA and Clustering
 - Used perent of C methylation of filtered samples
 - 0.05 p-value threshold of F-statistics of mean/var difference
 - 5 clusters given methylation patterns 
 
-![filtered_correlation]( results/2025-04-11/filtered_anova_cor.png )
+![filtered_correlation]( filtered_anova_cor.png )
 
-![filtered_heatmap]( results/2025-04-11/filtered_anova_heatmap.png )
+![filtered_heatmap]( filtered_anova_heatmap.png )
 
 ## Annotation Per cluster
 '> $o
 
 for f in $odir/filtered*anno.tsv;do
     n=` echo $f | grep -o "cluster\d" `
-    echo "- [$n]($f)" >> $o
+    echo "- [$n](${f##*/})" >> $o
 done
 
